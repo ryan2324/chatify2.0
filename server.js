7a14c48acad62e4f23bf59bd53eabed99a84b203
unknown <jayryan17380@gmail.com>
2022-07-30 17:11:37 +0800

    make it better

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const search = require('./routes/search-user');
 require('dotenv').config();
 const app = express();
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT;
 const server = app.listen(PORT, () =>{
     console.log(`listening on port: ${PORT}`)
 })
